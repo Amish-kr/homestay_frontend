a5a087eb8d35c228098d5c460d6378b23810251c
deepanurag <deepanurag420@gmail.com>
2024-01-12 17:33:12 +0530

    styled login page

--- src/Pages/login/login.jsx
+++ src/Pages/login/login.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
 import "./login.css";
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -35,12 +34,11 @@ const Login = () => {
         );
         const data = await response.json();
         if (response.status === 200) {
+          window.location.href = "/";
         } else {
           alert(data.message);
           console.log(data.message);
         }
-
-        window.location.href = "/";
       } catch (error) {
         console.error("Error:", error.message);
       }
@@ -51,37 +49,99 @@ const Login = () => {
   };
 
   return (
-    <div className="container">
-      <div className="right-section">
-        <h2 className="right">Login</h2>
-        <p>Please Enter your email and password</p>
-        <form className="login-form" onSubmit={handleSubmit}>
-          <label htmlFor="email">Email</label>
-          <input
-            type="text"
-            id="email"
-            placeholder="Email"
-            value={email}
-            onChange={handleEmailChange}
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        {/* <img
+          className="mx-auto h-10 w-auto"
+          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+          alt="Your Company"
+        /> */}
+        <svg
+          className="mx-auto h-10 w-auto"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205 3 1m1.5.5-1.5-.5M6.75 7.364V3h-3v18m3-13.636 10.5-3.819"
           />
-          <div className="password-box">
-            <label htmlFor="password">Password</label>
+        </svg>
+
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Sign in to your account
+        </h2>
+      </div>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        {/* <p>Please Enter your email and password</p> */}
+        <form className="space-y-6" onSubmit={handleSubmit} method="POST">
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Email address
+          </label>
+          <div className="mt-2">
             <input
-              type="password"
-              id="password"
-              placeholder="Password"
-              value={password}
-              onChange={handlePasswordChange}
+              type="text"
+              id="email"
+              name="email"
+              autoComplete="email"
+              required
+              className="block w-full rounded-md border-0 py-1.5 px-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              placeholder="Email"
+              value={email}
+              onChange={handleEmailChange}
             />
           </div>
-
-          <button className="button1" type="submit">
-            Login
-          </button>
-          <div className="Account">
-            Don't have an account? <Link to="/register">Sign up</Link>
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Password
+              </label>
+            </div>
+            <div className="mt-2">
+              <input
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 px-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="Password"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+            </div>
           </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Sign in
+            </button>
+          </div>
+
+          {/* <div className="Account">
+            Don't have an account? <Link to="/register">Sign up</Link>
+          </div> */}
         </form>
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Don't have an account?{" "}
+          <Link
+            to="/register"
+            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+          >
+            Sign Up
+          </Link>
+        </p>
       </div>
     </div>
   );
